7d6830f62055c6d63ab800c781e192cfb40ac3ad
mchorney <m.chorney7@gmail.com>
2017-07-20 15:02:37 -0500

    Added 5 javascript files

---
+++ declare.js
@@ -0,0 +1,19 @@
+var num = 28.2;
+var add = 50;
+var sub = 23.9;
+var mul = 1;
+
+var f_name = "James";
+var l_name = "Bond";
+var m_name = "Francine";
+
+var codingIsFun = true;
+var codingIsLame = false;
+
+var fullname = "";
+
+console.log(num + add + sub + mul);
+console.log(l_name, m_name, f_name);
+console.log(codingIsFun);
+console.log(codingIsLame);
+console.log(fullname);
---
+++ rangePrint.js
@@ -0,0 +1,8 @@
+function rangePrint(num1,num2,num3) {
+
+  while(num1 < num2){
+    console.log(num1);
+    num1 += num3;}
+}
+
+rangePrint(2,10,2)
